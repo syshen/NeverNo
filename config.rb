eaf962d1bb04b5b80ba94103cf2b3b02076e6a1b
syshen <sysheen@gmail.com>
2013-03-10 11:34:18 +0800

    initial commit

---
+++ config.rb
@@ -0,0 +1,50 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+http_generated_images_path = "../images"
+javascripts_dir = "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
+
+sass_options = {:debug_info=>false} # by Fire.app 
+asset_cache_buster :none
+
+line_comments = false # by Fire.app 
+
+
+# Make a copy of sprites with a name that has no uniqueness of the hash.
+on_sprite_saved do |filename|
+  if File.exists?(filename)
+    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+  end
+end
+
+# Replace in stylesheets generated references to sprites
+# by their counterparts without the hash uniqueness.
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+    end
+  end
+end
